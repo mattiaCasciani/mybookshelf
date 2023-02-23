64e3f4372655f6974bd24ede5e50dc4162dd4bdb
mattia casciani <89260051+mattiaCasciani@users.noreply.github.com>
2023-02-23 20:02:29 +0100

    creata una pagina base di dashboard

---
+++ dashboard.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="it" data-theme="light">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/pico.min.css"> 
+    <title>Document</title>
+</head>
+<body>
+
+    <style>
+        .header{
+            display:inline-flex;
+            margin-top:5px;
+        }
+        .bookshelf{
+            height:80px;
+        }
+        .title__header{
+            font-size:40px;
+            vertical-align: middle;
+            transform: translateY(+15%);
+        }
+    </style>
+
+
+
+        <div class="header">
+            <img src="bookshelf.png" href="https://minecraft.fandom.com/wiki/Bookshelf" class="bookshelf">
+                <h1 class="title__header">MyBookshelf</h1>
+        </div>
+</body>
+</html>
