8449fe803a1e58c34ed03a744427bf60380f8a91
mattia casciani <89260051+mattiaCasciani@users.noreply.github.com>
2023-02-23 20:01:48 +0100

    apportati cambiamenti grafici sulla pagina di login

--- login.php
+++ login.php
@@ -13,30 +13,75 @@
 ?>
 
 <!DOCTYPE html>
-<html>
+<html data-theme="light">
     <head>
         <meta charset="UTF-8">
-        <link rel="stylesheet" href="https://unpkg.com/@picocss/pico@1.*/css/pico.min.css">
+        <link rel="stylesheet" href="css/pico.min.css"> 
         <title></title>
     </head>
     <body>
-        <h1>LOGIN<h1/>
-        <form action="index.php" method="post">
-            <div class="grid">
-                <label for="E-mail">
-                    E-mail
-                    <input type="text" id="e-mail" name="e-mail" placeholder="e-mail" required>
-                    <span class="text-danger"><?php if (isset($email_error)) echo $email_error; ?></span>
-                </label>
-                
-                <label for="Password">
-                    Password
-                    <input type="password" id="password" name="password" placeholder="password" required>
-                    <span class="text-danger"><?php if (isset($password_error)) echo $password_error; ?></span>
-                </label>
-                
-                <button type="submit">Invia</button>
-            </div>
-        </form>
+
+
+    <style>
+        .header{
+            width:100%;
+            display:inline-flex;
+            margin-top:5px;
+        }
+        .title{
+            width:100%;
+            text-align:center;
+        }
+        .container__form{
+            display:block;
+            max-width: 800px;
+            margin:auto;
+        }
+        .container{
+            transform: translateY(50%);
+        }
+        .bookshelf{
+            height:80px;
+        }
+        .title__header{
+            font-size:40px;
+            vertical-align: middle;
+            transform: translateY(+15%);
+        }
+        .button{
+            position:absolute;
+            height:60px;
+            margin:10px;
+            right:0px;
+        }
+    </style>
+
+
+
+
+        <div class="header">
+            <img src="bookshelf.png" href="https://minecraft.fandom.com/wiki/Bookshelf" class="bookshelf">
+            <h1 class="title__header">MyBookshelf</h1>
+            <a href="index.php" role="button" class="button">indietro</a>
+        </div>
+        <div class="container">
+            <h1 class="title">Login</h1>
+            <form action="dashboard.php" method="post"class="container__form">
+                <div class="container__box">
+                    <label for="E-mail">
+                        E-mail
+                        <input type="text" id="e-mail" name="e-mail" placeholder="e-mail" required>
+                        <span class="text-danger"><?php if (isset($email_error)) echo $email_error; ?></span>
+                    </label>
+                    
+                    <label for="Password">
+                        Password
+                        <input type="password" id="password" name="password" placeholder="password" required>
+                        <span class="text-danger"><?php if (isset($password_error)) echo $password_error; ?></span>
+                    </label>
+                    <button type="submit">Invia</button>
+                </div>
+            </form>
+        </div>
     </body>
 </html>
