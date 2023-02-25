26b85912183b1bd7173309fa147f57f37ee84e5c
mattia casciani <89260051+mattiaCasciani@users.noreply.github.com>
2023-02-25 08:44:33 +0100

    apportate modifiche alla pagina dashboard
    aggiunta una barra di ricerca e una zona per l'inserimento dei file, non ancora usabile

--- dashboard.php
+++ dashboard.php
@@ -10,9 +10,16 @@
 <body>
 
     <style>
+        body{
+            min-height:100vh;
+        }
         .header{
             display:inline-flex;
-            margin-top:5px;
+            padding-top:5px;
+            position:fixed;
+            width:100%;
+            background: white; 
+            height:95px;
         }
         .bookshelf{
             height:80px;
@@ -22,13 +29,41 @@
             vertical-align: middle;
             transform: translateY(+15%);
         }
+        .bar{
+            max-width:60%;
+            margin-top:15px;
+            position: absolute;
+            right:50%;
+            transform: translateX(+50%);
+        }
+
+        .file{
+            width:200px;
+            height:200px;
+        }
+        
+        .container__file{
+            position:absolute;
+            border:1px solid red;
+            min-height:70vh;
+            margin-top:95px;
+            width:100%;
+        }
     </style>
 
 
 
         <div class="header">
             <img src="bookshelf.png" href="https://minecraft.fandom.com/wiki/Bookshelf" class="bookshelf">
-                <h1 class="title__header">MyBookshelf</h1>
+            <h1 class="title__header">MyBookshelf</h1>
+            <input type="text" id="cerca" name="cerca" placeholder="cerca" class="bar">
+        </div>
+        
+        
+        <div class="container__file">
+            <div class="grid">
+                
+            </div>
         </div>
 </body>
 </html>
