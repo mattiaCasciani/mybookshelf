19038062d3007a7a835ca3165b5da5196de39eae
mattia casciani <89260051+mattiaCasciani@users.noreply.github.com>
2023-02-27 20:24:37 +0100

    aggiornato dashboard
    ora gestisce l'immissione e download dei file, reso graficamente migliore e fixato il problema dei file in una sola riga

--- dashboard.php
+++ dashboard.php
@@ -42,6 +42,7 @@
             width:100%;
             height:95px;
             background:white;
+            z-index:999;
         }
         .bookshelf{
             height:80px;
@@ -53,7 +54,7 @@
         }
         .bar{
             max-width:60%;
-            margin-top:15px;
+            margin-top:10px;
             position: absolute;
             right:50%;
             transform: translateX(+50%);
@@ -66,41 +67,94 @@
         
         .container__file{
             position:absolute;
-            border:1px solid red;
             min-height:70vh;
             margin-top:95px;
             width:100%;
         }
         
         .icon{
-            border:1px solid green;
-            height:80px;
-            position:relative;
-            transform: translateX(20%);
+            min-width:100px;
+            max-width:100px;
+        }
+
+        .row{
+            margin-left:50%;
+            transform: translateX(-50%);
+            border:auto;
+            justify-content:center;
+            display:inline-flex;
+        }
+
+        .img__box{ 
+            padding:10px;
+            margin:25px 15%;
+            text-align:center;
+        }
+
+        .upload{
+            position:absolute;
+            right: 0px;
+            width:150px;
+            margin: 10px;
+            height:60px;
+            top:0px;
+        }
+
+        .uploadFile{
+            position:absolute;
+            right:160px;
+            max-width:100px;
+            height:60px;
+            top:0px;
+            margin:10px;
         }
     </style>
 
 
 
-        <div class="header">
-            <img src="bookshelf.png" href="https://minecraft.fandom.com/wiki/Bookshelf" class="bookshelf">
-            <h1 class="title__header">MyBookshelf</h1>
-            <input type="text" id="cerca" name="cerca" placeholder="cerca" class="bar">
-        </div>
+    <div class="header">
+        <a href="index.php"><img src="bookshelf.png" class="bookshelf"></a>
+        <a href="index.php" style="text-decoration:none;"><h1 class="title__header">MyBookshelf</h1></a>
+        <input type="text" id="cerca" name="cerca" placeholder="cerca - funzionerà nei prossimi aggiornamenti" class="bar"> 
+        <form method="POST" action="upload.php" enctype="multipart/form-data">
+            <label for="file-upload"><input type="file" id="file-upload" name="uploadedFile" class="uploadFile"></label>
+            <button type="submit" name="uploadBtn" value="Upload" class="upload" >carica</button>
+        </form>
+    </div>
         
         
-        <div class="container__file">
-            <div class="grid">
+    <div class="container__file">
+        <?php
+                    
+        $a=2;
+            while ($a < sizeof($files)) { ?>
+                <div class="row">
                 <?php
-                    for ($i=0; $i < sizeof($files)*3; $i++) { 
-                        ?>
-                        <div style="max-width:100px; border:1px solid yellow;">
-                            <img class="icon" src="img/JPG.svg">
+                for ($i=0; $i < 5; $i++) { 
+                    
+                    if ($a < sizeof($files)) {
+                    ?>
+                    
+                        <div class="img__box">
+                            <a href="download.php?path=<?php echo 'cartelle/'.$email.'/'.$files[$a];?>"><img class="icon" src=<?php $tmp=substr($files[$a],(strlen($files[$a])-3));
+                                                        $tmp=strtoupper($tmp);
+                                                        $imgs = scandir("img/");
+                                                        if(in_array($tmp.".svg",$imgs))
+                                                            echo "img/".$tmp.".svg";
+                                                        else
+                                                            echo "img/DOC.svg";?>></a>
+                            <p><?php echo substr($files[$a],0);?></p>
                         </div>
-                        <?php
+                    
+                    <?php
                     }
-                ?>
-            </div>
-        </div>
+                    $a++;
+                }
+                echo "</div>";
+                echo "<br>";
+            }    
+        ?>
+    </div>
+    <script src="script.js"></script>
 </body>
 </html>
