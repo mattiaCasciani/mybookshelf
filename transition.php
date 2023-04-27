a4dfea5cc4044e765be591a25d4b92265067192e
mattia casciani <89260051+mattiaCasciani@users.noreply.github.com>
2023-04-27 10:33:09 +0200

    creato il file di transizione

---
+++ transition.php
@@ -0,0 +1,29 @@
+<?php
+    header( "refresh:2;url=login.php" );
+?>
+
+<!DOCTYPE html >
+<html lang="it" data-theme="light">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/pico.min.css">
+    <title>login</title>
+    <link rel="icon" type="image/x-icon" href="bookshelf.png">
+</head>
+<body>
+
+    <style>
+        .txt{
+            position:absolute;
+            top:50%;
+            left:50%;
+            transform: translateY(-50%) translateX(-50%);
+            text-align:center;
+        }
+    </style>
+
+    <p class="txt">account creato con successo, <br> verrai reindirizzato alla pagina di login!</p>
+</body>
+</html>
